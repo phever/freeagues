30b1f06dfda9a8efebb8d3c2f167f5fbc3544c64
Mike Penner <mike.pnnr@crimsonquality.com>
2019-01-07 21:14:28 -0700

    updated for 2019 leagues and placeholders for cedh

--- freeagues/example_settings_secrets.py
+++ freeagues/example_settings_secrets.py
@@ -5,13 +5,9 @@ SECRET_KEY = 'a_really_long_string'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-### EMAIL STUFF
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_HOST_USER = 'my_luggage'
-# EMAIL_HOST_PASSWORD = '12345'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-
-### OR
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = '/mnt/project/emails' # change this to a proper location
+# EMAIL STUFF
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'deepflix'
+EMAIL_HOST_PASSWORD = '12345'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
